dd3bb9554f1ae6cf8ea37f04781aed32c5e73ce2
Bruno Barbieri <brunobar79@gmail.com>
2020-09-23 18:58:49 -0400

    deploy

---
+++ precache-manifest.6e7615b0c766791c7aca350c0d94753c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfc5e987880ca27deddd72781a3ca8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d43078061fdd2650b2c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a874b41631b05df2ab3d",
+    "url": "/static/js/2.332313ec.chunk.js"
+  },
+  {
+    "revision": "6c8e53ce03150b081d1cb79e3c6fde3c",
+    "url": "/static/js/2.332313ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d43078061fdd2650b2c",
+    "url": "/static/js/main.db8bb1e8.chunk.js"
+  },
+  {
+    "revision": "b79157635837ddff4f0a",
+    "url": "/static/js/runtime-main.c4e28f79.js"
+  }
+]);

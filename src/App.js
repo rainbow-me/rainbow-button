9afe2f76075ee3052c96a31bba02920e457d93d0
Esteban Miño <efmino@uc.cl>
2021-07-21 12:53:03 -0400

    onClientInitialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import ConnectV2 from './components/ConnectV2'
+import ConnectV2 from './components/ConnectV2.tsx'
 import './App.css';
 import { SUPPORTED_MAIN_CHAINS } from './utils/chainsConstants';
 
@@ -19,6 +19,10 @@ function App() {
           url: 'https://rainbow.me',
           icons: ['https://avatars2.githubusercontent.com/u/48327834?s=200&v=4'],
         }}
+        methods={[
+          "eth_sendTransaction", "personal_sign", "eth_signTypedData"
+        ]}
+        onClientInitialized={() => console.log('Client initialized!')}
       />
     </div>
   );
---
+++ src/components/ConnectV2.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useCallback, useEffect, useState } from 'react';
+import WalletConnectClient, { CLIENT_EVENTS } from "@walletconnect/clientv2";
+import '../App.css';
+import { constructDeeplink } from '../helpers/deeplink';
+import { PairingTypes } from '@walletconnect/typesv2';
+
+function ConectButtonV2({
+    chainId,
+    metadata,
+    methods,
+    onClientInitialized,
+    relayProvider,
+}: { chainId: string, relayProvider: string, metadata: object, methods: string[], onClientInitialized: (client: WalletConnectClient) => void }) {
+    const [uri, setUri] = useState<string>();
+
+    const goToDeeplink = useCallback(() => {
+        if (!uri) return
+        window.location.href = uri!
+    }, [uri])
+
+    const connectToRainbow = useCallback(() => goToDeeplink(), [goToDeeplink]);
+
+    useEffect(() => {
+        const walletConnectInit = async () => {
+            const client = await WalletConnectClient.init({
+                relayProvider,
+                metadata
+            });
+            client.on(
+                CLIENT_EVENTS.pairing.proposal,
+                async (proposal: PairingTypes.Proposal) => {
+                    const { uri } = proposal.signal.params;
+                    const deeplink = constructDeeplink(uri)
+                    setUri(deeplink)
+                },
+            );
+            onClientInitialized(client);
+            await client.connect({
+                permissions: {
+                    blockchain: {
+                        chains: [chainId],
+                    },
+                    jsonrpc: {
+                        methods,
+                    },
+                },
+            });
+
+        }
+        walletConnectInit()
+    }, [chainId, metadata, methods, onClientInitialized, relayProvider]);
+
+    return (
+        <div className="App" >
+            <a
+                className="button"
+                onClick={connectToRainbow}
+                rel="noopener noreferrer"
+            >
+                {`🌈  Connect to Rainbow`}
+            </a>
+        </div>
+    );
+}
+
+export default ConectButtonV2;

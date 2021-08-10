3da0c41247aa59d2289422caf6d7e1aec4528654
Esteban Miño <efmino@uc.cl>
2021-08-09 20:24:55 -0400

    config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,30 +1,12 @@
-const postcss = require('rollup-plugin-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 const images = require('@rollup/plugin-image');
-const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config, options) {
-    config.plugins.push(
-      postcss({
-        plugins: [
-          autoprefixer(),
-          cssnano({
-            preset: 'default',
-          }),
-        ],
-        inject: false,
-        // only write out CSS for the first bundle (avoids pointless extra files):
-        extract: !!options.writeMeta,
-      }),
-      images({ include: ['src/images/rainbow-logo.png', '**/*.jpg'] }),
-      copy({
-        targets: [
-          { src: 'src/images/**/*', dest: 'dist/public' }
-        ]
-      })
-    );
-    return config;
+    config.plugins = [
+      images({ incude: ['**/*.png', '**/*.jpg'] }),
+      ...config.plugins,
+    ]
+
+    return config
   },
-};
+}

04947e19aab13f280977e072523f89f70cded05b
Esteban Miño <efmino@uc.cl>
2021-08-11 17:59:59 -0400

    renderv1

--- example/Dapp.tsx
+++ example/Dapp.tsx
@@ -3,15 +3,13 @@ import * as React from 'react';
 import './App.css';
 import { useState, useMemo } from 'react';
 import DappV1 from './dapps/v1'
-import DappV2 from './dapps/v2'
 import { Button, Wrapper } from './styled';
 
 const Dapp = () => {
-  const [version, setVersion] = useState('')
+  const [version, setVersion] = useState('v1')
 
   const renderDapp = useMemo(() => {
     if (version === 'v1') return <DappV1 /> 
-    return <DappV2 /> 
   }, [version])
 
   return (
@@ -23,7 +21,6 @@ const Dapp = () => {
           <h2 className="text-center">Choose a RainbowButton</h2>
             <Wrapper>
                   <Button key={'v1'} onClick={() => setVersion('v1')}>{'RainbowButton WC v1'}</Button>
-                  <Button key={'v2'} onClick={() => setVersion('v2')}>{'RainbowButton  WC v2'}</Button>
             </Wrapper>
           </div>
         )}

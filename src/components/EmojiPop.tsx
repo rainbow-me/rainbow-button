c55c3c007bc5893f015a9cd3325c86ce1eb7c883
Esteban Miño <efmino@uc.cl>
2021-08-13 11:09:48 -0400

    animations

--- src/components/EmojiPop.tsx
+++ src/components/EmojiPop.tsx
@@ -71,7 +71,7 @@ class Fountain {
       { passive: false }
     );
 
-    document
+    document?.getElementById(RAINBOW_BUTTON_ID)
       ?.addEventListener(tap, (e: MouseEvent | TouchEvent) => {
         this.mouseX = e instanceof MouseEvent ? e.pageX : e.touches[0].pageX;
         this.mouseY = e instanceof MouseEvent ? e.pageY : e.touches[0].pageY;

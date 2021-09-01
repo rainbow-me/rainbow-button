1c3e6e459a85c033cbb602ac29195ebad56c6bc6
Bret Comnes <bcomnes@gmail.com>
2021-09-01 16:50:29 -0600

    lint --fix
    Plus a few eslint disable comments

--- example/dapps/constants/index.tsx
+++ example/dapps/constants/index.tsx
@@ -1,17 +1,34 @@
 export const SUPPORTED_METHODS = [
-  "eth_sendTransaction", "personal_sign", "eth_signTypedData"
-]
+  'eth_sendTransaction',
+  'personal_sign',
+  'eth_signTypedData',
+];
 
 export const supportedMainChainsInfoEip155 = {
-  'eip155:1': { name: "Ethereum Mainnet", color: '#0E76FD'},
-  'eip155:10': { name: "Optimism", color: '#FF4040'},
-  'eip155:137': { name: "Polygon", color: '#8247E5'},
-  'eip155:42161': { name: "Arbitrum", color: '#2D374B'},
-}
+  'eip155:1': { name: 'Ethereum Mainnet', color: '#0E76FD' },
+  'eip155:10': { name: 'Optimism', color: '#FF4040' },
+  'eip155:137': { name: 'Polygon', color: '#8247E5' },
+  'eip155:42161': { name: 'Arbitrum', color: '#2D374B' },
+};
 
 export const supportedMainChainsInfo = {
-  '1': { name: "Ethereum", color: '#0E76FD', value: "ethereum", iconWidth: "20"},
-  '10': { name: "Optimism", color: '#FF4040', value: "optimism", iconWidth: "42"},
-  '137': { name: "Polygon", color: '#8247E5', value: "polygon", iconWidth: "42"},
+  '1': {
+    name: 'Ethereum',
+    color: '#0E76FD',
+    value: 'ethereum',
+    iconWidth: '20',
+  },
+  '10': {
+    name: 'Optimism',
+    color: '#FF4040',
+    value: 'optimism',
+    iconWidth: '42',
+  },
+  '137': {
+    name: 'Polygon',
+    color: '#8247E5',
+    value: 'polygon',
+    iconWidth: '42',
+  },
   // '42161': { name: "Arbitrum", color: '#2D374B', value: "arbitrum", iconWidth: "42"},
-}
+};
--- example/dapps/helpers/accounts.tsx
+++ example/dapps/helpers/accounts.tsx
@@ -1,35 +1,31 @@
-
 export async function formatTestTransactionExperimental(account: string) {
-    const [, chainId, address] = account.split(":");
-
-    // gasLimit
-    const gasLimit = '0x5208'
-  
-    // value
-    const value = '0x0'
-  
-    const tx = { from: address, to: address, data: "0x", gasLimit, value };
-  
-    return tx;
-  }
-
-
-export async function formatTestTransaction(address: string) {
+  const [, , /*chainId*/ address] = account.split(':');
 
   // gasLimit
-  const gasLimit = '0x5208'
+  const gasLimit = '0x5208';
 
   // value
-  const value = '0x0'
+  const value = '0x0';
 
-  const tx = { from: address, to: address, data: "0x", gasLimit, value };
+  const tx = { from: address, to: address, data: '0x', gasLimit, value };
+
+  return tx;
+}
+
+export async function formatTestTransaction(address: string) {
+  // gasLimit
+  const gasLimit = '0x5208';
+
+  // value
+  const value = '0x0';
+
+  const tx = { from: address, to: address, data: '0x', gasLimit, value };
 
   return tx;
 }
 
 export const renderAddress = (address = '') => {
-  return [
-    address.substring(0, 4),
-    address.substring(address.length - 4),
-  ].join('...')
-}
+  return [address.substring(0, 4), address.substring(address.length - 4)].join(
+    '...'
+  );
+};
